a4f0bd644784f47ff5ce9d3cc32eda0a24052f02
serhangursoy <gursoyserhan@yandex.com>
2017-03-30 15:44:04 +0300

    Fresh Start
    Everything in one place.

---
+++ iaprojectform/iaprojectform/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace iaprojectform
+{
+    partial class StarterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.questionBox = new MetroFramework.Controls.MetroLabel();
+            this.questionBox2 = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = this;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // questionBox
+            // 
+            this.questionBox.AutoSize = true;
+            this.questionBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.questionBox.CustomBackground = true;
+            this.questionBox.CustomForeColor = true;
+            this.questionBox.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.questionBox.ForeColor = System.Drawing.Color.OldLace;
+            this.questionBox.Location = new System.Drawing.Point(476, 60);
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(116, 25);
+            this.questionBox.Style = MetroFramework.MetroColorStyle.Green;
+            this.questionBox.TabIndex = 0;
+            this.questionBox.Text = "Left to Right;";
+            // 
+            // questionBox2
+            // 
+            this.questionBox2.AutoSize = true;
+            this.questionBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.questionBox2.CustomBackground = true;
+            this.questionBox2.CustomForeColor = true;
+            this.questionBox2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.questionBox2.ForeColor = System.Drawing.Color.OldLace;
+            this.questionBox2.Location = new System.Drawing.Point(476, 357);
+            this.questionBox2.Name = "questionBox2";
+            this.questionBox2.Size = new System.Drawing.Size(133, 25);
+            this.questionBox2.Style = MetroFramework.MetroColorStyle.Green;
+            this.questionBox2.TabIndex = 1;
+            this.questionBox2.Text = "Top to bottom;";
+            // 
+            // StarterForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.Controls.Add(this.questionBox2);
+            this.Controls.Add(this.questionBox);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.HelpButton = true;
+            this.Name = "StarterForm";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroForm.MetroFormShadowType.DropShadow;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "PuzzleKillaz";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroLabel questionBox2;
+        private MetroFramework.Controls.MetroLabel questionBox;
+    }
+}
+
---
+++ iaprojectform/iaprojectform/Form1.cs
@@ -0,0 +1,169 @@
+﻿using MetroFramework.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace iaprojectform
+{
+    public partial class StarterForm : MetroFramework.Forms.MetroForm
+    {
+       
+        private String INITIALIZER_FILE = "init.ini";
+        private int LEFT_MARGIN = 20, TOP_MARGIN = 20, SIZE = 70, SQUARE_COUNT, GAP = 20;
+        private MetroLabel[,] SQUARES;
+        private String[,] ANSWERS_SQUARE;
+        private int[] hor_start;  // Indicates the horizontal line's starting square. 
+
+
+        private Color C_Sq_Avail = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+        private Color C_Not_Avail = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(176)))), ((int)(((byte)(137)))));
+
+        public StarterForm()
+        {
+            InitializeComponent();
+
+            questionBox.Text += "\r\n";
+            questionBox2.Text += "\r\n";
+
+            PuzzleInitializer();
+            questionBox.Location = new Point(SQUARE_COUNT*SIZE + LEFT_MARGIN*2, TOP_MARGIN + SIZE);
+            //questionBox.MaximumSize = new Size(this.Width - (SQUARE_COUNT * SIZE + LEFT_MARGIN * 2) - LEFT_MARGIN, questionBox.Size.Height);
+            questionBox.Size = new Size(this.Width - (SQUARE_COUNT * SIZE + LEFT_MARGIN * 2) - LEFT_MARGIN ,questionBox.Size.Height);
+
+            questionBox2.Location = new Point(SQUARE_COUNT * SIZE + LEFT_MARGIN * 2, TOP_MARGIN + SIZE + questionBox.Height+GAP);
+           // questionBox2.MaximumSize = new Size(this.Width - (SQUARE_COUNT * SIZE + LEFT_MARGIN * 2) - LEFT_MARGIN, questionBox.Size.Height);
+            questionBox2.Size = new Size(this.Width - (SQUARE_COUNT * SIZE + LEFT_MARGIN * 2) - LEFT_MARGIN, questionBox.Size.Height);
+
+            questionBox.ForeColor = Color.White;
+            questionBox2.ForeColor = Color.White;
+            questionBox.BackColor = C_Sq_Avail;
+            questionBox2.BackColor = C_Sq_Avail;
+        }
+
+
+        private bool PuzzleInitializer()
+        {
+            // Read the input file. Create and assign squares accordingly.
+            SQUARE_COUNT = 0;
+
+            int counter = 1;
+            string line;
+
+            // Read the file and display it line by line.
+            StreamReader file = new StreamReader(INITIALIZER_FILE);
+            while ((line = file.ReadLine()) != null)
+            {
+                int sqrLine = 1;
+                SQUARE_COUNT = Int32.Parse(line);
+                Console.WriteLine("We have "+SQUARE_COUNT+"x"+SQUARE_COUNT+" squares!");
+                // Create Label Array
+                SQUARES = new MetroLabel[SQUARE_COUNT,SQUARE_COUNT];
+                ANSWERS_SQUARE = new String[SQUARE_COUNT,SQUARE_COUNT];
+
+                for (int a = 0; a < SQUARE_COUNT; a++) {
+                    for (int j = 0; j < SQUARE_COUNT; j++){
+                        ANSWERS_SQUARE[a,j] = null; } }
+
+                line = file.ReadLine();  // Skip # char.
+                for (int a = 0;  a < SQUARE_COUNT;  a++){
+                    line = file.ReadLine(); // Now we have X X X X X like a string
+                    string[] tokens = line.Split(' ');
+
+                    // Lets start to create our labels.
+                    for (int k = 0; k < tokens.Length; k++) {
+                        Console.Write(tokens[k] + " ");
+                        MetroLabel tmp = new MetroLabel();
+                        tmp.Style = MetroFramework.MetroColorStyle.Red;
+                        tmp.CustomBackground = true;
+                        tmp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                        tmp.Location = new Point(k * SIZE + LEFT_MARGIN, sqrLine * SIZE + TOP_MARGIN);
+                        tmp.Size = new Size(SIZE, SIZE);
+                        if (Int32.Parse(tokens[k]) == 0) {  // Create a black square
+                            tmp.BackColor = C_Not_Avail;
+                           // Console.WriteLine("Row " + (sqrLine-1) + " Column " + k + " is BLACK!");
+                            ANSWERS_SQUARE[sqrLine-1,k] = "*"; // Set its Answer to TAB?.
+                        } else {
+                            tmp.BackColor = C_Sq_Avail;
+                        }
+                        SQUARES[sqrLine-1,k] = tmp;
+                        this.Controls.Add(tmp);
+                    }
+                    Console.WriteLine();
+                    sqrLine++;
+                }
+
+                line = file.ReadLine();  // Skip # char.
+
+                // Now lets read our questions and our answers.
+                // First, horizontal questions.
+                sqrLine = 1;
+                for (int a = 0; a < SQUARE_COUNT; a++)
+                {
+                    line = file.ReadLine(); // Now we have XX|A B C
+                    string[] tokens = line.Split('|');  // Now we have "XX" and "A B C"
+                    // TODO Set question.
+                    questionBox.Text += sqrLine + ": "+ tokens[0] + "\r\n";
+                    string[] squareAnswers = tokens[1].Split(' ');
+                    int ansCount = 0;
+                    for(int k = 0;  k < SQUARE_COUNT; k++) {
+                        // If its not BLACK
+                        if (ANSWERS_SQUARE[sqrLine - 1, k] == null) {
+                        //    Console.WriteLine("Added *" + squareAnswers[ansCount] + "* to row " + (sqrLine-1) + " and to column " + k );
+                            ANSWERS_SQUARE[sqrLine - 1, k] = squareAnswers[ansCount];
+                            ansCount++; 
+                        }
+                    }
+
+                    sqrLine++;
+                }
+
+                line = file.ReadLine();  // Skip # char.
+                // Now lets read our questions and our answers.
+                // First, horizontal questions.
+                sqrLine = 1;
+                for (int a = 0; a < SQUARE_COUNT; a++)
+                {
+                    line = file.ReadLine(); // Now we have XX|A B C
+                    string[] tokens = line.Split('|');  // Now we have "XX" and "A B C"
+                    // TODO Set question.
+                    questionBox2.Text += sqrLine + ": " + tokens[0] + "\r\n";
+                    sqrLine++;
+                }
+
+                // Lets print answers 
+                for (int a = 0; a < SQUARE_COUNT; a++) {
+                    for (int j = 0; j < SQUARE_COUNT; j++){
+                        Console.Write(ANSWERS_SQUARE[a, j]);
+                    } 
+                    Console.WriteLine();
+                }
+
+            }
+
+            file.Close();
+
+            // Suspend the screen.
+            Console.ReadLine();
+
+
+            /*
+            this.sqr11.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.sqr11.CustomBackground = true;
+            this.sqr11.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.sqr11.Location = new System.Drawing.Point(23, 117);
+            this.sqr11.Name = "metroLabel1";
+            this.sqr11.Size = new System.Drawing.Size(80, 80);
+            this.sqr11.TabIndex = 0;
+            this.sqr11.Text = "sqr11";
+            */
+
+
+            return true;
+        }
+
+    }
+}
